70ec0af8327ada97e75cc2060779bf20294f7479
ChriZStormy <christian.gomezmagdaleno@yahoo.com>
2026-03-11 18:53:31 -0600

    Agrego y actualizo Base de Datos con DBcontext

--- VehiculosAPI/Program.cs
+++ VehiculosAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using VehiculosAPI.Data;
 using VehiculosAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IVehiculoService, VehiculoService>();
 var app = builder.Build();
 
